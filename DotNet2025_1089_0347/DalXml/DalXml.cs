fe8970feec2c4e1d5723198e7d9f0f32d9d90901
ester-meyer <mindy05832@gmail.com>
2025-04-30 22:32:55 +0300

    XML-Implementation

---
+++ DotNet2025_1089_0347/DalXml/DalXml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dal
+{
+    internal class DalXml:IDal
+    {
+
+    }
+}
